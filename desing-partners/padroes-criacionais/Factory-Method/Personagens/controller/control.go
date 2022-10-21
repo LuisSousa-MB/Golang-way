4b87e2def5935edd152432761c24fe5d53958b03
LuisSousa-MB <luis.sousa@mb.com.br>
2022-10-21 18:38:37 -0300

    Personagens.Control.go comentarios sobre as funcs

--- desing-partners/padroes-criacionais/Factory-Method/Personagens/controller/control.go
+++ desing-partners/padroes-criacionais/Factory-Method/Personagens/controller/control.go
@@ -7,35 +7,12 @@ import (
 	"time"
 )
 
-func GeradorDePersonagem(name string) *models.IPersonagem {
+func GeradorDePersonagem(name string) *models.IPersonagem { //Publica. Gerencia a ordem das funções, cria e retorna o personagem.
 	fmt.Println("Gerando novo personagem...")
 	novoPersongem := defineClasse(randomizadorPersonagem())
 	return novoPersongem
 }
-func ExibePersonagem(p models.IPersonagem) {
-	fmt.Println()
-	fmt.Println("Personagem gerado!!!")
-	fmt.Println()
-	fmt.Printf("Nome: %s", p.GetName())
-	fmt.Println()
-	fmt.Printf("Classe: %s", p.GetClass())
-	fmt.Println()
-	fmt.Printf("HP: %d", p.GetHP())
-	fmt.Println()
-	fmt.Printf("MP: %d", p.GetMP())
-	fmt.Println()
-	fmt.Printf("Poder: %d", p.GetPoder())
-	fmt.Println()
-	fmt.Printf("Força: %d", p.GetForca())
-	fmt.Println()
-	fmt.Printf("Agilidade: %d", p.GetAgilidade())
-	fmt.Println()
-	fmt.Printf("Level: %d", p.GetLevel())
-	fmt.Println()
-	fmt.Printf("XP: %d", p.GetXP())
-	fmt.Println()
-}
-func randomizadorPersonagem() (name string, hp, mp, poder, forca, agilidade, level, xp int) {
+func randomizadorPersonagem() (name string, hp, mp, poder, forca, agilidade, level, xp int) { // Cria de maneira randomica o nome e os atributos para o personagem
 	rand.Seed(time.Now().UnixNano())
 	names := [30]string{"Gobu", "Taos", "Bekye", "Cyeno", "Geada", "Ankey", "Wuema", "Rofes", "Pezea",
 		"Tetel", "Cles", "Tifaytal", "Zeage", "Keate", "Lupia", "Ulel", "Curumu", "Rulas", "Caferius", "Sebasth", "Lorpheus", "Mizandre", "Saujin",
@@ -84,7 +61,7 @@ func randomizadorPersonagem() (name string, hp, mp, poder, forca, agilidade, lev
 	return name, hp, mp, poder, forca, agilidade, level, xp
 }
 
-func defineClasse(name string, hp, mp, poder, forca, agilidade, level, xp int) *models.IPersonagem {
+func defineClasse(name string, hp, mp, poder, forca, agilidade, level, xp int) *models.IPersonagem { //Retorna persoagem completo e define sua classe de acordo com a distribuição de pontos
 	rand.Seed(time.Now().UnixNano())
 	equilibriumx := 2
 	if forca > agilidade && forca > poder {
@@ -121,11 +98,34 @@ func defineClasse(name string, hp, mp, poder, forca, agilidade, level, xp int) *
 	}
 }
 
-func equilibrium(alfa, beta int) (maior, menor int) {
+func equilibrium(alfa, beta int) (maior, menor int) { //Corrige possivel desequilibrio da distribuição de pontos conforme a classe
 	rand.Seed(time.Now().UnixNano())
 	equilibrium := rand.Intn(5) + 2
 	fmt.Println("Equilibrando distribuição em:", equilibrium)
 	beta -= equilibrium
 	alfa += equilibrium
 	return alfa, beta
+}
+func ExibePersonagem(p models.IPersonagem) { // Exibe os dados do personagem criado
+	fmt.Println()
+	fmt.Println("Personagem gerado!!!")
+	fmt.Println()
+	fmt.Printf("Nome: %s", p.GetName())
+	fmt.Println()
+	fmt.Printf("Classe: %s", p.GetClass())
+	fmt.Println()
+	fmt.Printf("HP: %d", p.GetHP())
+	fmt.Println()
+	fmt.Printf("MP: %d", p.GetMP())
+	fmt.Println()
+	fmt.Printf("Poder: %d", p.GetPoder())
+	fmt.Println()
+	fmt.Printf("Força: %d", p.GetForca())
+	fmt.Println()
+	fmt.Printf("Agilidade: %d", p.GetAgilidade())
+	fmt.Println()
+	fmt.Printf("Level: %d", p.GetLevel())
+	fmt.Println()
+	fmt.Printf("XP: %d", p.GetXP())
+	fmt.Println()
 }
